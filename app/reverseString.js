d971a2ddd2987d04eca41d77175545abd78fb219
Charpell <charpellumeh@gmail.com>
2017-05-18 10:52:20 -0700

    Write logic code for reverseString.js

---
+++ app/reverseString.js
@@ -0,0 +1,33 @@
+let reverseString  = function (str){
+	let temp = '';
+	let i = str.length;
+	isTrue = true;
+	
+
+	if (str === ''){
+		return null;
+
+	}
+	if (checkPalindrom(str)){
+		return isTrue;
+			
+	} else {
+
+			while(i > 0){
+			temp += str.substring(i - 1, i);
+			i--;
+		}
+		return temp;
+	
+	}
+
+
+
+	function checkPalindrom(str){
+	 	return str == str.split('').reverse().join('');
+	 }
+
+}
+
+
+module.exports = reverseString;
