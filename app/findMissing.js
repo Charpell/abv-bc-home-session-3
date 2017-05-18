4d95755a5fad2ed5b3c2191ebbc4a6d3d9630c69
Charpell <charpellumeh@gmail.com>
2017-05-18 13:12:53 -0700

    Refactor findMissing.js

--- app/findMissing.js
+++ app/findMissing.js
@@ -6,18 +6,19 @@ let findMissing  = function (arr1,arr2){
 	let difference = []; 
 
 	if (arr1.length === arr2.length){
+		
 		difference.push(0);
 		difference = parseInt(difference.join());
 	}
 
 	else{
 
-		for(let i=0;i<arr2.length;i++){
+		for(let i = 0; i < arr2.length; i++){
 
-		   if(arr1.indexOf(arr2[i])==-1){
+		   if(arr1.indexOf(arr2[i]) === -1){
+
 		   	difference.push(arr2[i]);
 		   	difference = parseInt(difference.join());
-
 
 	   		}
 		}
